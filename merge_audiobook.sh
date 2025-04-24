174b94d42075a64c177876ca4f5f848a78b2417e
J. Greg Williams <283704+totallyGreg@users.noreply.github.com>
2025-04-24 12:22:59 -0400

    Lot of improvements in timing and report
    still not sure why total time does not add up large list merges

--- merge_audiobook.sh
+++ merge_audiobook.sh
@@ -1,49 +1,58 @@
 #!/bin/bash
 set -euo pipefail
 
-# Check for input files
-# NOTE: this currently assumes m4a/b audiofiles
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <file1.m4a> <file2.m4a> ..."
-  exit 1
-fi
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NC=$(tput sgr0)
 
-# # Create safe temporary directory
-tmpdir=$(mktemp -d -t audiomerge)
-
-# Create
-ramdisk_setup() {
-  set -x
-  ram_size=$((1024 * 2048)) # 1GB
-  ramdisk=$(hdiutil attach -nomount ram://$ram_size)
-  diskutil erasevolume APFSI "AudioRAM" "${ramdisk#/dev/}"
-  tmpdir=$(mktemp -d -p "/Volumes/AudioRAM" -t audiomerge)
-  echo "Created /Volumes/AudioRAM"
-}
-
-# Remove
-ramdisk_clean() {
-  diskutil unmount /Volumes/AudioRAM
-  # hdiutil detach "$(diskutil list | awk '/AudioRAM/ {print $NF}')" -force
-  hdiutil eject ${ramdisk#/dev/}
-  unset ramdisk
-}
-
-# converted_pipe="$tmpdir/converted_file"
-# mkfifo "$converted_pipe"
-# output_dir=$(dirname "$1")
-output_dir=$(pwd)
-cleanup_needed=true
-
-ok() { echo -e '\033[1;32m'"$1"'\033[0m'; }
-warn() { echo -e '\033[1;33m'"$1"'\033[0m'; }
+ok() { echo -e "${GREEN}$1${NC}"; }
+warn() { echo -e "${YELLOW}$1${NC}"; }
 error() {
-  echo -e '\033[1;31mERROR: '"$1"'\033[0m'
+  echo -e "${RED}ERROR: $1${NC}"
   logger "$1"
 }
 
+# Check for input files
+# NOTE: this currently assumes m4a/b audiofiles
+if [ $# -lt 1 ]; then
+  warn "Usage: $0 (audio_files || Directory of audio_files)"
+  exit 1
+fi
+
+setup_temp() {
+  # # Create safe temporary directory
+  tmpdir=$(mktemp -d -t audiomerge-"$artist")
+
+  # Create
+  ramdisk_setup() {
+    set -x
+    ram_size=$((1024 * 2048)) # 1GB
+    ramdisk=$(hdiutil attach -nomount ram://$ram_size)
+    diskutil erasevolume APFSI "AudioRAM" "${ramdisk#/dev/}"
+    tmpdir=$(mktemp -d -p "/Volumes/AudioRAM" -t audiomerge)
+    echo "Created /Volumes/AudioRAM"
+    set +x
+  }
+
+  # Remove
+  ramdisk_clean() {
+    diskutil unmount /Volumes/AudioRAM
+    # hdiutil detach "$(diskutil list | awk '/AudioRAM/ {print $NF}')" -force
+    hdiutil eject ${ramdisk#/dev/}
+    unset ramdisk
+  }
+
+  # converted_pipe="$tmpdir/converted_file"
+  # mkfifo "$converted_pipe"
+  # output_dir=$(dirname "$1")
+  output_dir=$(pwd)
+  cleanup_needed=true
+}
+
 # Set conditional trap
-trap '[[ $cleanup_needed == "true" ]] && { ok "Cleaning up"; rm -rf "$tmpdir"; } || { ok "Cleaning up"; ramdisk_clean; }' EXIT INT TERM
+trap '[[ $cleanup_needed == "true" ]] && { ok "Cleaning up"; rm -rf "$tmpdir"; }' EXIT INT TERM
+# trap '[[ $cleanup_needed == "true" ]] && { ok "Cleaning up"; rm -rf "$tmpdir"; } || { ok "Cleaning up"; ramdisk_clean; }' EXIT INT TERM
 
 # optimized conversion for any audio type
 convert_to_m4b() {
@@ -73,12 +82,28 @@ audio_to_caf() {
   # NOTE: experiment to try and take any audio and pass through highest quality
   local input_file=${1}
   local output_file="${2:-${input_file%.*}.caf}"
-  # Converts MP3 → CAF (Apple's core audio format)
-  # Generates loudness metadata (--soundcheck-generate + --anchor-generate)
+  local afconvert_args=()
+
+  # NOTE:
+  # Converts <any? audio> → CAF (Apple's core audio format)
   # Preserves original audio quality (-d 0 = no data format conversion)
-  afconvert "$input_file" -o "${output_file}" -d 0 -f caff \
-    --soundcheck-generate \
+  # Generates loudness metadata (--soundcheck-generate + --anchor-generate)
+  # afconvert "$input_file" -o "${output_file}" -d 0 -f caff \
+  #   --soundcheck-generate \
+  #   --anchor-generate \
+  #   --anchor-loudness \
+  #   --generate-hash
+  afconvert_args+=(
+    "$input_file"
+    -o "${output_file}"
+    -d 0 -f caff
+    --soundcheck-generate
     --anchor-generate
+  )
+  # --generate-hash # WARN: Error: ExtAudioFileSetProperty ('cfmt') failed ('fmt?')
+
+  # Execute with proper quoting
+  afconvert "${afconvert_args[@]}"
 
   # echo "${output_file}"
 }
@@ -86,11 +111,18 @@ audio_to_caf() {
 caf_to_m4b() {
   local input_file=${1}
   local output_file="${2:-${input_file%.caf}.m4b}"
-  afconvert -f m4bf -d aac -q 127 -s 3 \
-    --soundcheck-read \
-    --media-kind "Audiobook" \
-    "${input_file}" -o "${output_file}"
 
+  # NOTE: highest quality conversion from caf file
+  afconvert_args+=(
+    "${input_file}"
+    -f m4bf -d aac -q 127 -s 3
+    --soundcheck-read
+    --media-kind "Audiobook"
+    -o "${output_file}"
+  )
+  # --copy-hash # NOTE: no point until generate-hash works
+
+  afconvert "${afconvert_args[@]}"
   echo "${output_file}"
 }
 
@@ -131,11 +163,11 @@ audio_caf_m4b_pipeline() {
 # TODO: this should be called recursively but MUST be sorted first
 generate_chapter_info() {
   local meta_file="$1"
-  local offset="$2"
+  local real_offset="$2"
 
   # Single JSON parse with type conversion
   local chapter_data
-  chapter_data=$(jq -r --argjson offset "$offset" '
+  chapter_data=$(jq -r --argjson offset "$real_offset" '
     def sec2ms: (. | tonumber) * 1000 | floor;
 
     (.chapters | length > 0) as $has_chapters |
@@ -157,7 +189,8 @@ generate_chapter_info() {
       "END=\($start_time_ms + $offset + $duration_ms)\n" +
       "title=\($filename_base)\n"
     end
-  ' "$meta_file")
+  ' <<<"$meta_file")
+  # ' "$meta_file")  # NOTE: removed when I switched to storing json in variable
 
   printf "%s\n" "$chapter_data"
 }
@@ -188,23 +221,21 @@ gather_file_info() {
   # ffmetadata=$(ffprobe -v quiet -i "$input_file" -show_chapters -show_format -of json)
   # ffprobe -v quiet -i "$input_file" -show_chapters -show_format -of json >"$ffmetadata"
 
-  # NOTE: This works but creates extra files
-  meta_file="$tmpdir/${file}.json"
-  ffprobe -v quiet -i "$file" -show_chapters -show_format -of json >"$meta_file"
-  [[ -z $artist ]] && artist=$(jq -r '.format.tags.artist' "$meta_file")
-  [[ -z $album ]] && album=$(jq -r '.format.tags.album' "$meta_file")
-  [[ -z $year ]] && year=$(jq -r '.format.tags.date' "$meta_file")
-  file_duration=$(jq -r '.format.duration' "$meta_file")
-  # For integer milliseconds (rounded):
-  rounded_ms=$(sec2msInt $file_duration)
+  # # NOTE: This works but creates extra files
+  # meta_file="$tmpdir/${file}.json"
+  # ffprobe -v quiet -i "$file" -show_chapters -show_format -of json >"$meta_file"
+  # [[ -z $artist ]] && artist=$(jq -r '.format.tags.artist' "$meta_file")
+  # [[ -z $album ]] && album=$(jq -r '.format.tags.album' "$meta_file")
+  # [[ -z $year ]] && year=$(jq -r '.format.tags.date' "$meta_file")
+  # file_duration=$(jq -r '.format.duration' "$meta_file")
 
   # HACK: trying to save data to variable instead of to file
-  # meta_file=$(ffprobe -v quiet -i "$input_file" -show_chapters -show_format -of json)
-  # [[ -z ${artist} ]] && artist=$(jq -r '.format.tags.artist' <<<"$meta_data")
-  # [[ -z ${album} ]] && album=$(jq -r '.format.tags.album' <<<"$meta_data")
-  # [[ -z ${year} ]] && year=$(jq -r '.format.tags.date' <<<"$meta_data")
-  #
-  # file_duration=$(jq -r '.format.duration' <<<"$meta_file")
+  meta_data=$(ffprobe -v quiet -i "$input_file" -show_chapters -show_format -of json)
+  [[ -z ${artist} ]] && artist=$(jq -r '.format.tags.artist' <<<"$meta_data")
+  [[ -z ${album} ]] && album=$(jq -r '.format.tags.album' <<<"$meta_data")
+  [[ -z ${year} ]] && year=$(jq -r '.format.tags.date' <<<"$meta_data")
+
+  real_file_duration_secs=$(jq -r '.format.duration' <<<"$meta_data")
   # cat "$ffmetadata"
 }
 
@@ -213,9 +244,10 @@ process_file() {
   local file="$1"
   local mime_type
   local aac_file
-  local meta_file
+  local meta_data
 
-  echo "Processing $file..."
+  # echo -en "Processing $file...\t"
+  printf '%s\t' "Processing ${file}.."
   start_timestamp=$(date +%s)
   # meta_file="$tmpdir/${file}.json"
   mime_type=$(file -b --mime-type "$file")
@@ -225,9 +257,10 @@ process_file() {
     aac_file="$file"
   elif [[ "$mime_type" =~ $AUDIO_MP3 ]] || [[ "$file" =~ $FILE_MP3 ]]; then
     # NOTE: MP3 files are converted to m4b before adding to ffmpeg_file_merge_list
+    # setup_temp
     local converted_file
     gather_file_info "$file"
-    echo "Converting MP3: $file to M4B:"
+    printf '%5s\t' "->m4b"
     converted_file=$(convert_to_m4b "$file")
     aac_file="$converted_file"
   else
@@ -242,13 +275,22 @@ process_file() {
   # file_duration=$(jq -r '.format.duration' "$meta_file")
 
   # NOTE: then generate any chapter information
-  generate_chapter_info "$meta_file" "$cumulative_offset" >>"$chapters_file"
+  # generate expects a real number for cumulative_offset and will do the rounding in jq
+  generate_chapter_info "$meta_data" "$cumulative_offset" >>"$chapters_file"
+
+  # For integer milliseconds (rounded):
+  rounded_ms=$(sec2msInt $real_file_duration_secs)
+  # ok "$(basename "$aac_file")\t start: $cumulative_offset\t real: $real_file_duration_secs\t rounded: $rounded_ms"
+  # printf '%-15s %-20s %-20s %-15s\n' "$(basename "$aac_file")" "start: $cumulative_offset" "real: $real_file_duration_secs" "rounded: $rounded_ms"
+  printf '%-20s %-20s %-15s\n' "start: $cumulative_offset" "real: $real_file_duration_secs" "rounded: $rounded_ms"
 
   # NOTE: and finally increment offset and storage arrays
-  cumulative_offset=$((cumulative_offset + rounded_ms)) # NOTE: set each file offset to running total of duration_ms as integer.
+  # result=$( echo $num1+$num2 | bc )
+  # cumulative_offset=$((cumulative_offset + real_file_duration_secs)) # NOTE: set each file offset to running total of duration_ms as integer.
+  cumulative_offset=$(echo $cumulative_offset + $real_file_duration_secs | bc)
   valid_files+=("$aac_file")
-  durations+=("$file_duration") # NOTE: Store total file durations in seconds with full precision and compare later as milliseconds
-  meta_files+=("$meta_file")    # NOTE: Store meta data file location in array
+  durations+=("$real_file_duration_secs") # NOTE: Store total file durations in seconds with full precision and compare later as milliseconds
+  meta_files+=("$meta_data")              # NOTE: Store meta data file location in array
 
   # end_timestamp=$(date +%s)
   # elapsed_time=$(expr $end_timestamp - $start_timestamp) # WARN: this seems to stop processing of the function
@@ -277,6 +319,10 @@ process_audiobook() {
   artist=""
   year=""
   cumulative_offset=0
+
+  # NOTE: really only needs to be setup if doing a transcode
+  # but ffmetadata is currently stored there too
+  setup_temp
 
   # Create file list (with proper quoting)
   file_merge_list="$tmpdir/file_merge_list.txt"
@@ -395,10 +441,17 @@ merge_m4a_files() {
   # and required tags and chapters created with generate_ffmetadata
   generate_ffmetadata
 
+  local sum_seconds=$(
+    IFS=+
+    echo "scale=6; ${durations[*]}" | bc
+  )
+
   # NOTE: `-movflags use_metadata_tags` doesn't seem to actually keep the global metadata
   # NOTE: testing composing the ffmpeg command in a function
 
   echo "==========================================="
+  printf '\t\t\t%-20s %-20s\n' "Offset: $cumulative_offset" "Real: $sum_seconds"
+
   echo "Merging $(cat $file_merge_list | wc -l) files..."
   # Use an array to store command components
   local ffmpeg_args=()
@@ -443,8 +496,8 @@ sec2msInt() {
 
 get_file_duration_in_ms() {
   local file="$1"
-  file_duration=$(ffprobe -v quiet -i "$file" -show_format -of json | jq -r '.format.duration')
-  duration_ms=$(sec2msInt $file_duration)
+  real_file_duration_secs=$(ffprobe -v quiet -i "$file" -show_format -of json | jq -r '.format.duration')
+  duration_ms=$(sec2msInt $real_file_duration_secs)
   echo "$duration_ms"
 }
 
@@ -452,13 +505,13 @@ get_file_duration_in_ms() {
 verify_merged_file() {
   # compare final cumulative_offset to sum of durations which should be the same as final file duration
   #
-  merged_duration=$(get_file_duration_in_ms "$tmpdir/merged.m4a")
-  if [ $? -ne 0 ] || [ -z "$merged_duration" ] || [ "$merged_duration" -eq 0 ]; then
+  merged_duration_ms=$(get_file_duration_in_ms "$tmpdir/merged.m4a")
+  if [ $? -ne 0 ] || [ -z "$merged_duration_ms" ] || [ "$merged_duration_ms" -eq 0 ]; then
     echo "Error: Failed to get valid duration for merged file" >&2
     exit 1
   fi
   # merged_duration=$(get_file_duration_in_ms "$tmpdir/merged.m4a")
-  expected_duration=0
+  expected_duration_ms=0
 
   # Sum durations using bc (scale=6 for 6 decimal places)
   sum_seconds=$(
@@ -466,18 +519,20 @@ verify_merged_file() {
     echo "scale=6; ${durations[*]}" | bc
   )
   echo "Total seconds of all files: $sum_seconds" # Output: 381.457415
-  echo "Merged file duration in ms: $merged_duration"
+  echo "Merged file duration in ms: $merged_duration_ms"
 
-  expected_duration=$(sec2msInt $sum_seconds)
-  warn "Expected duration in ms: $expected_duration"
+  expected_duration_ms=$(sec2msInt $sum_seconds)
+  warn "Expected duration in ms: $expected_duration_ms"
 
   # Allow for a small margin of error (e.g., 1 second = 1000 ms)
-  margin=1000
-  difference=$((merged_duration - expected_duration))
+  margin=5000 # HACK: Increased margin, not sure why estimates are off
+  difference=$((merged_duration_ms - expected_duration_ms))
   difference_abs=${difference#-} # Remove minus sign if present
 
   if [ "$difference_abs" -gt "$margin" ]; then
-    warn "Warning: Merged file duration ($merged_duration ms) differs significantly from sum of input durations ($expected_duration ms)" >&2
+    warn "Warning: Merged file duration ($merged_duration_ms ms) differs by $difference_abs from sum of input durations ($expected_duration_ms ms)" >&2
+    cleanup_needed=false
+    open "$tmpdir"
     exit 1
   else
     ok "Duration verification passed: ${difference_abs}ms within $margin ms margin"
